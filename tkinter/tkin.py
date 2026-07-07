d1dccec27204b34f693cca87f2e5747a3bdbc6fd
Amanjain1974 <aj1329652@gmail.com>
2026-07-07 12:21:41 +0530

    tkinter button

--- tkinter/tkin.py
+++ tkinter/tkin.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.font as tfont
+from tkinter import ttk
 
 
 window = tk.Tk()
@@ -10,5 +11,26 @@ label =tk.Label(text="Hello world \n\n have a nice day")
 label.config(font=("Roman",25))
 
 label.pack()
+# label["text"] ="have a nice day"
+# label.config(text ="My new App")
+counter =0
+def function_button():
+    # print("Thanks For Clicking")
+    label.config(text =f"The user input is {user_input}")
+
+# taking user input using entry
+user_input =tk.Entry(width =30,show = "*")
+user_input.pack()
+
+
+# buttons
+button =tk.Button(text="click",command=function_button)
+# this prints on console
+button.pack()
+
+# quit button
+quit_button = ttk.Button(text="Quit",command= window.destroy)
+quit_button.pack()
+
 
 window.mainloop()
